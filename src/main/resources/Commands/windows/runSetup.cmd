1adbe54f685023b7ddcc5a86e89472030bc30a05
Raimund Hocke <rmhdevelop@me.com>
2013-11-06 09:59:42 +0100

    added parameter to java command in runSetup

--- src/main/resources/Commands/windows/runSetup.cmd
+++ src/main/resources/Commands/windows/runSetup.cmd
@@ -5,7 +5,7 @@ set SIKULIX_HOME=%~dp0
 echo +++ trying to start Sikuli Setup in %SIKULIX_HOME%
 PATH=%SIKULIX_HOME%libs;%PATH%
 
-java -jar "%SIKULIX_HOME%sikuli-setup-1.1.jar"
+java -jar "%SIKULIX_HOME%sikuli-setup-1.1.jar" %*
 
 ENDLOCAL
 pause
